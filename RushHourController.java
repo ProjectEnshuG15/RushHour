e9b7d98fbdb694ac030234d167846f70ae54e22b
yuta39 <xoggkllo00@gmail.com>
2015-07-08 15:12:35 +0900

    Mainで作成したJFrameインスタンスをContorollerに受け渡す処理を追記しました。

--- RushHourController.java
+++ RushHourController.java
@@ -1,11 +1,17 @@
+
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 class RushHourController{
 
-  public RushHourController(){
+  public RushHourController(JFrame rushflame){
     this.run();
   }
 
   private void run(){
-    
+
   }
 
 }
--- RushHourMain.java
+++ RushHourMain.java
@@ -1,5 +1,27 @@
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 class RushHourMain{
   public static void main(String[] args) {
-    new RushHourController();
+    RushHourFrame rushframe = new RushHourFrame("RushHour",100, 100, 400, 300);
+    new RushHourController(rushframe);
+  }
+
+  static class RushHourFrame extends JFrame{
+    RushHourFrame(String title, int xorig, int yorig, int w, int h){
+      setTitle(title);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      JPanel p = new JPanel();
+      p.setSize(w, h);
+      p.setLayout(null);
+      getContentPane().add(p);
+      setVisible(true);
+      Insets insets = getInsets();
+      setBounds(xorig, yorig,
+      w + insets.left + insets.right, h + insets.top + insets.bottom);
+      setResizable(false);
+    }
   }
 }
