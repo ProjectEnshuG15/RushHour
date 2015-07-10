ef81dc8fb994b8b4ff2b1e492c39ddd350a5b6d4
yuta39 <xoggkllo00@gmail.com>
2015-07-10 18:09:18 +0900

    JFrameのテストを完了しました。

--- RushHourController.java
+++ RushHourController.java
@@ -3,10 +3,16 @@ import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.BorderLayout;
 
 class RushHourController{
 
-  public RushHourController(JFrame rushflame){
+  public RushHourController(RushHourFrame rushflame){
+    JLabel label1 = new JLabel("今日の天気：");
+    JPanel p = new JPanel();
+    p.add(label1);
+    rushflame.getContentPane().add(p,BorderLayout.CENTER);
+    rushflame.setVisible(true);
     this.run();
   }
 
