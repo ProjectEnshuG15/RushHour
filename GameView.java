009ce5e665660b9c83b109cefd387b8e04538219
yuta39 <xoggkllo00@gmail.com>
2015-07-10 16:34:56 +0900

    Viewのひな形作成しました

---
+++ GameView.java
@@ -0,0 +1,40 @@
+import java.awt.Point;
+import javax.swing.*;
+
+class GameView extends View{
+  private int heightMasuNum = 50;
+  private int weightMasuNum = 50;
+  private Point cpuPoint[] = null;
+  private Point playerPoint = null;
+  private int moveCount = 0;
+  private Point soundButtonPoint = null;
+  private String name = null;
+  private int score = 0;
+  private Point undoPoint = null;
+  private Point menuPoint = null;
+
+  public GameView(){
+
+  }
+
+  public void soudChage(){
+
+  }
+
+  public void moveObstacle(){
+
+  }
+
+  public void clickUndo(){
+
+  }
+
+  public void clickMenuButton(){
+
+  }
+
+  @Override
+  public void paint(){
+
+  }
+}
--- RankingView.java
+++ RankingView.java
@@ -1,3 +1,22 @@
-class RankingView extends View {
+import java.awt.Point;
+import javax.swing.*;
 
+class RankingView extends View {
+  private Point rogoPoint = null;
+  private Point topRankPoint = null;
+  private Point topNamePoint = null;
+  private Point topScorePoint = null;
+
+  public RankingView(){
+
+  }
+
+  public void clickGoButton(){
+
+  }
+
+  @Override
+  public void paint(){
+    
+  }
 }
