9a3101dd370ad10028f65046856fe6b804cc2fbe
yuta39 <xoggkllo00@gmail.com>
2015-07-10 16:06:14 +0900

    ひな形作成しました

---
+++ ScoreModel.java
@@ -0,0 +1,13 @@
+import javax.swing.ImageIcon;
+
+class ScoreModel{
+  private ImageIcon nextButtonImage = null;
+
+  public ScoreModel{
+
+  }
+
+  public ImageIcon getNextButtonImage(){
+    return nextButtonImage;
+  }
+}
