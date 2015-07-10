55643e7d69f50d7764c73b7f3480ccabf3e227c7
Taga Hayato <g1344766@cse.kyoto-su.ac.jp>
2015-07-10 18:04:55 +0900

    コントローラの概形
    コントローラーの概形を作成しました

--- RushHourController.java
+++ RushHourController.java
@@ -6,12 +6,63 @@ import javax.swing.*;
 
 class RushHourController{
 
-  public RushHourController(JFrame rushflame){
-    this.run();
-  }
+    public RushHourController(JFrame rushflame){
+        this.run();
+    }
 
-  private void run(){
+    private void run(){
 
-  }
+    }
 
+    public void moveInputNameView() {
+
+    }
+
+    public void moveRankingView() {
+
+    }
+
+    public void moveStageSelect(String username) {
+        
+    }
+
+    public void changeStageInfo(int d) {
+
+    }
+
+    public void moveGameView() {
+
+    }
+
+    public void speakerOnOff() {
+
+    }
+
+    public void moveMenuView() {
+
+    }
+
+    public void moveTitleView() {
+
+    }
+
+    public void resumeGame() {
+
+    }
+
+    public void clickPiece() {
+
+    }
+
+    public void movePiece() {
+
+    }
+
+    public void partPiece() {
+
+    }
+
+    public void moveContinueView() {
+
+    }
 }
