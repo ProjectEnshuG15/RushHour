7788e00ca463e468dd0900795ee8babdbb080098
yuta39 <xoggkllo00@gmail.com>
2015-07-15 13:33:33 +0900

    とりあえず今で来てるところ

--- GameModel.java
+++ GameModel.java
@@ -2,30 +2,45 @@ import javax.swing.ImageIcon;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 class GameModel{
-  private ImageIcon speakerButtonImage[] = null;
   private ImageIcon undoButtonImage = null;
   private ImageIcon menuButtonImage = null;
-  private BufferedReader stageInformation = null;
+  /*フィールド内容の変更*/
+  private ArrayList<String[]> stageInformation = new ArrayList();
+  private ArrayList<ImageIcon> speakerButtonImage = new ArrayList();
+  /*新規のフィールド*/
+  private ImageIcon playerImage = null;
+  private ArrayList<ImageIcon> cpuImage = new ArrayList();
 
   public GameModel(){
-    this.speakerButtonImage[0] = new ImageIcon("./logo/speaker.png");
-    this.speakerButtonImage[1] = new ImageIcon("./logo/speaker.png");
+    this.speakerButtonImage.add(new ImageIcon("./logo/speaker.png"));
+    this.speakerButtonImage.add(new ImageIcon("./logo/speakerOFF.png"));
     this.undoButtonImage = new ImageIcon("./logo/UNDOButton.png");
-    this.menuButtonImage = new ImageIcon("./logo/logo3.tiff");
+    this.menuButtonImage = new ImageIcon("./logo/menuButton.png");
+    this.playerImage = new ImageIcon("./sozai/vehicle/car_carrier.png");
+    this.cpuImage.add(new ImageIcon("./sozai/vehicle/car_pink.png"));
+    this.cpuImage.add(new ImageIcon("./sozai/vehicle/car_skyblue.pngg"));
+    this.cpuImage.add(new ImageIcon("./sozai/vehicle/car_yellow.png"));
+    this.cpuImage.add(new ImageIcon("./sozai/vehicle/car_yellowgreen.png"));
+    this.cpuImage.add(new ImageIcon("./sozai/vehicle/syoubousya_pump.png"));
 
     try {
           FileReader fr = new FileReader("./data/stage.csv");
-          this.stageInformation = new BufferedReader(fr);
+          BufferedReader br = new BufferedReader(fr);
+          String line="";
+          while ((line = br.readLine()) != null) {
+              String[] splitLine = line.split(",");
+              this.stageInformation.add(splitLine);
+          }
         }catch (IOException ex) {
             ex.printStackTrace();
         }
-
   }
 
   public ImageIcon getSpeakerButtonImage(int index){
-    return this.speakerButtonImage[index];
+    return this.speakerButtonImage.get(index);
   }
 
   public ImageIcon getUndoButtonImage(){
@@ -36,7 +51,7 @@ class GameModel{
     return this.menuButtonImage;
   }
 
-  public BufferedReader getStageInformation(){
+  public ArrayList getStageInformation(){
       return this.stageInformation;
   }
 
@@ -46,6 +61,14 @@ class GameModel{
 
   public void setScene(){
 
+  }
+
+  /*新規のメソッド*/
+  public ImageIcon getPlayerImage(){
+    return this.playerImage;
+  }
+  public ImageIcon getCpuImage(int index){
+    return this.cpuImage.get(index);
   }
 
 
--- GameView.java
+++ GameView.java
@@ -1,7 +1,24 @@
-import java.awt.Point;
-import javax.swing.*;
 
-class GameView extends View{
+import java.awt.*;
+import java.awt.geom.Line2D;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+class GameView extends View implements ActionListener{
+  /*  新規フィールド２つ　*/
+  private RushHourFrame rushframe = null;
+  private GameModel model = null;
+  private Draggable[] cpuLabel = null;
+  private Draggable playerLabel = null;
+  private Point goalPoint = null;
+
+  /* 旧フィールド */
   private int heightMasuNum = 50;
   private int weightMasuNum = 50;
   private Point cpuPoint[] = null;
@@ -13,8 +30,14 @@ class GameView extends View{
   private Point undoPoint = null;
   private Point menuPoint = null;
 
-  public GameView(){
-
+  public GameView(RushHourFrame f,GameModel m){
+    this.rushframe = f;
+    this.model = m;
+    ArrayList<String[]> list = m.getStageInformation();
+    this.goalPoint = new Point( Integer.parseInt(list.get(1)[0]),Integer.parseInt(list.get(1)[0]) );//ゴールの座標の設定
+    this.playerLabel = new Draggable(model.getPlayerImage(),0,3);
+    this.playerLabel.setMasuLocation(1,1);
+    this.name = "サンプル";
   }
 
   public void soudChage(){
@@ -35,6 +58,88 @@ class GameView extends View{
 
   @Override
   public void paint(){
+    JPanel p = new JPanel();
+    p.setLayout(null);
+    /*ボタンの設定*/
+    JButton undoButton = new ResizeJButton(model.getUndoButtonImage(),20,610,120,60);
+    undoButton.setActionCommand("undo");
+    JButton menuButton = new ResizeJButton(model.getMenuButtonImage(),370,610,120,60);
+    menuButton.setActionCommand("menu");
+    JButton speakerButton = new ResizeJButton(model.getSpeakerButtonImage(0),10,50,60,60);
+    speakerButton.setBorderPainted(false);
+    speakerButton.setActionCommand("speaker");
+    /*ラベルの設定*/
+    JLabel name_label = new JLabel(this.name + "さん");
+    name_label.setBounds(350,20,150,40);
+    name_label.setFont(new Font("Century", Font.ITALIC, 20));
+    JLabel socore_label = new JLabel("Score:");
+    socore_label.setBounds(330,50,100,50);
+    socore_label.setFont(new Font("Century", Font.ITALIC, 24));
+    /*ステージ用のJPanelの作成*/
+    JPanel stage = new GridJPanel();
+    stage.setPreferredSize(new Dimension(400,400));
+    stage.setBackground(Color.WHITE);
+    stage.setBounds(50,150,400,400);
+    stage.add(playerLabel);
+    /*ボタンへのイベントリスナーの追加*/
+    undoButton.addActionListener(this);
+		menuButton.addActionListener(this);
+    /*メインのパネルにコンポーネントを追加*/
+    p.add(undoButton);
+    p.add(menuButton);
+    p.add(speakerButton);
+    p.add(name_label);
+    p.add(socore_label);
+    p.add(stage);
+    rushframe.getContentPane().add(p);//FrameにPanelを追加
+    this.rushframe.setVisible(true);//描画処理
+  }
 
+  @Override
+  public void actionPerformed(ActionEvent ae) {
+    String action = ae.getActionCommand();
+    if(action.equals("undo")){
+      this.clickUndo();
+    }else if(action.equals("menu")){
+      clickMenuButton();
+    }else if(action.equals("speaker")){
+      soudChage();
+    }else{
+      System.out.println("out");
+    }
+  }
+
+  /* グリッド線描画のための新規クラス */
+  private class GridJPanel extends JPanel{
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+  			//	大きさ取得
+  			Dimension wh = this.getSize();
+        System.out.println(wh.width + ":" + wh.height);
+
+  			//グリッド線を惹く
+        for(int i=1;i<6;i++){
+          g.drawLine(wh.width/6*i,0,wh.width/6*i,wh.height);
+          g.drawLine(0,wh.height/6*i,wh.width,wh.height/6*i);
+        }
+
+    }
+  }
+}
+
+/**
+  新たに作成したクラス
+  デフォルトのJButtonでは画像ボタンのリサイズができないため
+  処理の簡略かのため作成
+*/
+class ResizeJButton extends JButton{
+
+  public ResizeJButton(ImageIcon icon,int x,int y,int w,int h){
+    Image new_img = icon.getImage().getScaledInstance(w,h,Image.SCALE_DEFAULT);
+    super.setIcon(new ImageIcon(new_img,""));
+    setBounds(x,y,w,h);
   }
 }
